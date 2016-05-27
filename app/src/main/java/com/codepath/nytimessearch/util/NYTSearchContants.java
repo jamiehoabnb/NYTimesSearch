84ca50d47f66260466711a2e1276d6c0394c0423
Jamie Ho <jamie.ho@airbnb.com>
2016-05-27 10:27:16 -0700

    Reached rate limit so switching API keys.

--- app/src/main/java/com/codepath/nytimessearch/util/NYTSearchContants.java
+++ app/src/main/java/com/codepath/nytimessearch/util/NYTSearchContants.java
@@ -6,5 +6,5 @@ public class NYTSearchContants {
 
     public static final String API_BASE_URL = "https://api.nytimes.com/";
 
-    public static final String API_KEY = "ac25876594df46d88d27fed52e4fa84c";
+    public static final String API_KEY = "19e3c9e45bac4faab2e1edad79d034f1";
 }
